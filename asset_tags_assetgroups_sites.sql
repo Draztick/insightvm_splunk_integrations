eacbc986e6943865c6337ca83f2e192d75f45520
Draztick <draztick@draztick.com>
2020-08-28 15:16:01 -0400

    added formating and capitalization for SQL formatting.

--- asset_tags_assetgroups_sites.sql
+++ asset_tags_assetgroups_sites.sql
@@ -1,41 +1,41 @@
-with "hostnames" as (
-    select distinct "da"."asset_id" "asset_id", RTRIM("da"."host_name", '.whs.int') "host_name"
-    from "dim_asset" "da"
-), "pKey" as (
-    select 
+WITH "hostnames" AS (
+    SELECT DISTINCT "da"."asset_id" "asset_id", RTRIM("da"."host_name", '.whs.int') "host_name"
+    FROM "dim_asset" "da"
+), "pKey" AS (
+    SELECT 
         "da"."asset_id" "asset_id",
-        concat(extract(epoch from "dsc"."finished"), "da"."asset_id"::text)::dec "checkpoint"
-    from "dim_asset" "da"
-        join "dim_site_asset" "dsa" using ("asset_id")
-        join "dim_site" "ds" using ("site_id")
-        join "dim_scan" "dsc" on "ds"."last_scan_id"="dsc"."scan_id"
+        CONCAT(EXTRACT(EPOCH FROM "dsc"."finished"), "da"."asset_id"::text)::dec "checkpoint"
+    FROM "dim_asset" "da"
+        JOIN "dim_site_asset" "dsa" USING ("asset_id")
+        JOIN "dim_site" "ds" USING ("site_id")
+        JOIN "dim_scan" "dsc" ON "ds"."last_scan_id"="dsc"."scan_id"
 )
-select 
-    distinct on ("da"."mac_address") "da"."mac_address" "mac",
+SELECT 
+    DISTINCT ON ("da"."mac_address") "da"."mac_address" "mac",
     "pKey"."checkpoint" "checkpoint",
-    string_agg(DISTINCT "da"."ip_address"::text, '|') ip,
-    string_agg(DISTINCT "hostnames"."host_name", '|') as "nt_host",
-    string_agg(distinct CONCAT("hostnames"."host_name", '.whs.int'), '|') "dns",
+    STRING_AGG(DISTINCT "da"."ip_address"::text, '|') "ip",
+    STRING_AGG(DISTINCT "hostnames"."host_name", '|') as "nt_host",
+    STRING_AGG(DISTINCT CONCAT("hostnames"."host_name", '.whs.int'), '|') "dns",
     "daos"."vendor" "os_vendor",
     "daos"."name" "os_name",
     "daos"."certainty" "certainty",
-    string_agg(DISTINCT "dst"."name", '|') "sites",
-    string_agg(DISTINCT "dag"."name", '|') "asset_groups",
-    string_agg(DISTINCT "dt"."name", '|') "tags",
+    STRING_AGG(DISTINCT "dst"."name", '|') "sites",
+    STRING_AGG(DISTINCT "dag"."name", '|') "asset_groups",
+    STRING_AGG(DISTINCT "dt"."name", '|') "tags",
     "dsc"."finished" "Last Scanned"
-from "postgres"."public"."dim_asset" "da"
-    join "hostnames" USING ("asset_id")
-    join "pKey" USING ("asset_id")
-    join "postgres"."public"."dim_asset_operating_system" "daos" using ("asset_id")
-    join "postgres"."public"."dim_site_asset" "dsa" USING ("asset_id")
-    join "postgres"."public"."dim_site" "dst" USING ("site_id")
-    join "postgres"."public"."dim_scan" "dsc" ON "dst"."last_scan_id"="dsc"."scan_id"
-    join "postgres"."public"."dim_asset_tag" "dat" USING ("asset_id")
-    join "postgres"."public"."dim_asset_group" "dag" USING ("asset_group_id")
-    join "postgres"."public"."dim_tag" "dt" USING ("tag_id")
-where
+FROM "postgres"."public"."dim_asset" "da"
+    JOIN "hostnames" USING ("asset_id")
+    JOIN "pKey" USING ("asset_id")
+    JOIN "postgres"."public"."dim_asset_operating_system" "daos" USING ("asset_id")
+    JOIN "postgres"."public"."dim_site_asset" "dsa" USING ("asset_id")
+    JOIN "postgres"."public"."dim_site" "dst" USING ("site_id")
+    JOIN "postgres"."public"."dim_scan" "dsc" ON "dst"."last_scan_id"="dsc"."scan_id"
+    JOIN "postgres"."public"."dim_asset_tag" "dat" USING ("asset_id")
+    JOIN "postgres"."public"."dim_asset_group" "dag" USING ("asset_group_id")
+    JOIN "postgres"."public"."dim_tag" "dt" USING ("tag_id")
+WHERE
     "pKey"."checkpoint" > ?
-group by 
+GROUP BY 
     "hostnames"."host_name",
     "pKey"."checkpoint",
     "da"."asset_id",
@@ -46,6 +46,6 @@ group by
     "daos"."name",
     "daos"."certainty",
     "dsc"."finished"
-order by
+ORDER BY
     "da"."mac_address" ASC,
     "pKey"."checkpoint" ASC
--- insight_vm_vulnerabilities.sql
+++ insight_vm_vulnerabilities.sql
@@ -1,34 +1,34 @@
-with "pKey" as (
-    select 
+WITH "pKey" AS (
+    SELECT 
         "da"."asset_id" "asset_id",
-        concat(extract(epoch from "fv"."first_discovered"), "da"."asset_id"::text)::dec "checkpoint"
-    from "postgres"."public"."dim_asset" "da"
-        join "postgres"."public"."fact_asset_vulnerability_instance" "favi" using ("asset_id")
-        join "postgres"."public"."dim_vulnerability" "dv" using ("vulnerability_id")
-        join "postgres"."public"."fact_vulnerability" "fv" using ("vulnerability_id")
+        concat(extract(epoch FROM "fv"."first_discovered"), "da"."asset_id"::text)::dec "checkpoint"
+    FROM "postgres"."public"."dim_asset" "da"
+        JOIN "postgres"."public"."fact_asset_vulnerability_instance" "favi" USING ("asset_id")
+        JOIN "postgres"."public"."dim_vulnerability" "dv" USING ("vulnerability_id")
+        JOIN "postgres"."public"."fact_vulnerability" "fv" USING ("vulnerability_id")
 )
-select 
-    distinct on ("da"."mac_address") "da"."mac_address" "mac",
+SELECT 
+    DISTINCT ON ("da"."mac_address") "da"."mac_address" "mac",
     "pKey"."checkpoint" "checkpoint",
     "da"."ip_address" "ip", 
     "da"."host_name" "hostname", 
     "dv"."title" "vulnerability", 
     "dv"."description" "description", 
-    substring("dv"."title", 'CVE-\d{4}-\d{1,}') "cve", 
-    substring("dv"."title", '[Mm]{1}[Ss]{1}\d{2}-\d{3,}') "ms",
+    SUBSTRING("dv"."title", 'CVE-\d{4}-\d{1,}') "cve", 
+    SUBSTRING("dv"."title", '[Mm]{1}[Ss]{1}\d{2}-\d{3,}') "ms",
     "dv"."severity" "severity",
     "dv"."cvss_score" "cvss",
     "favi"."proof" "signature",
     'insightvm' "dvc",
     "fv"."first_discovered" "discovered"
-from "postgres"."public"."dim_asset" "da"
-    join "pKey" using ("asset_id")
-    join "postgres"."public"."fact_asset_vulnerability_instance" "favi" using ("asset_id")
-    join "postgres"."public"."dim_vulnerability" "dv" using ("vulnerability_id")
-    join "postgres"."public"."fact_vulnerability" "fv" using ("vulnerability_id")
-where
+FROM "postgres"."public"."dim_asset" "da"
+    JOIN "pKey" USING ("asset_id")
+    JOIN "postgres"."public"."fact_asset_vulnerability_instance" "favi" USING ("asset_id")
+    JOIN "postgres"."public"."dim_vulnerability" "dv" USING ("vulnerability_id")
+    JOIN "postgres"."public"."fact_vulnerability" "fv" USING ("vulnerability_id")
+WHERE
     "pKey"."checkpoint" > ?
-group by
+GROUP BY
     "da"."mac_address",
     "pKey"."checkpoint",
     "da"."ip_address",
@@ -39,6 +39,6 @@ group by
     "dv"."cvss_score",
     "favi"."proof",
     "fv"."first_discovered"
-order by
+ORDER BY
     "da"."mac_address" ASC,
     "pKey"."checkpoint" ASC
